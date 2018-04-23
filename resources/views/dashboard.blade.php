8a7275ba8934fcbd7eb8a24406e63a274b32d878
Vikas <vikas@codosome.com>
2018-04-23 16:23:54 +0530

    make it better

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,7 +12,7 @@
       <div class="ui-app-frame" data-tg-refresh="ui-app-frame" id="ui-app-frame">
          <header class="ui-app-frame__header">
 		 <?php $avatar_background_color = '#7951B9'; 
-			$myvalue = 'Test me more';
+			$myvalue = 	Auth::user()->name;
 				$arr = explode(' ',trim($myvalue));
 			$avatar_initials = $arr[0];
 		 ?> 

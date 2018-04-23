c38882bb432a6096bf930ebf7a2ed76d9e81e2d9
Vikas <vikas@codosome.com>
2018-04-23 16:42:18 +0530

    make it better

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -69,6 +69,7 @@
                                  <span class="user-avatar__initials">
 									{{ $avatar_initials }}
                                  </span>
+								 <img alt="" class="gravatar gravatar--size-thumb" src=" @if( $avatar_url_small != '' ) {{ $avatar_url_small }} @else //proxy.shopifycdn.com/ab2694cdbecd03a84f6fd8f9eaf8d1e099b2c0d0f4e078408e60f0b9a0e4c422/www.gravatar.com/avatar/3fd238bd9cbf0330c770deb30c516e80.jpg?s=60&amp;d=blank @endif ">
                                  </span>
                               </div>
                               <div class="top-bar-profile__summary">
@@ -200,6 +201,7 @@
                            <span class="user-avatar__initials">
                            {{ $avatar_initials }}
                            </span>
+						   <img alt="" class="gravatar gravatar--size-thumb" src=" @if( $avatar_url_small != '' ) {{ $avatar_url_small }} @else //proxy.shopifycdn.com/ab2694cdbecd03a84f6fd8f9eaf8d1e099b2c0d0f4e078408e60f0b9a0e4c422/www.gravatar.com/avatar/3fd238bd9cbf0330c770deb30c516e80.jpg?s=60&amp;d=blank @endif ">
                            </span>
                         </div>
                         <div class="top-bar-profile__summary">

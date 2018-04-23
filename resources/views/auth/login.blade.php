61516db0ca448f2015b87759f70b9c6dd81da49d
Vikas <vikas@codosome.com>
2018-04-23 12:47:30 +0530

    make it better

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -54,18 +54,13 @@
 									</div>
 								</div>
 							</div>
-
-							<div class="form-group row mb-0">
-								<div class="col-md-8 offset-md-4">
-									<button type="submit" class="btn btn-primary">
+									<button type="submit" class="ui-button ui-button--primary ui-button--full-width dialog-submit">
 										{{ __('Login') }}
 									</button>
 
 									<a class="btn btn-link" href="{{ route('password.request') }}">
 										{{ __('Forgot Your Password?') }}
 									</a>
-								</div>
-							</div>
 						</form>
 					</div>
 				</div>

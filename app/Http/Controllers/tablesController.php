b80a460607aab2647563d1367e551fe8cad1a095
YoJeyThilip <thilipanyosanai@gmail.com>
2018-07-23 16:52:46 +0530

    changes

--- app/Http/Controllers/tablesController.php
+++ app/Http/Controllers/tablesController.php
@@ -12,63 +12,52 @@ use DB;
 
 class tablesController extends Controller
 { 
-	/**
-	* Create a new controller instance.
-	*
-	* @return void
-	*/
-	 
-    public function __construct()
-    {
-		$this->middleware('auth');
-    }
 	
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
-    {		
+    {	
+
+		$user_id = Auth::id();
+		
+		$email = UserMetaController::get_user_meta( $user_id, "printavo-email" );
+		
+		$api_token = UserMetaController::get_user_meta( $user_id, "printavo-api-token" );
+		
+		$printavo_status = UserMetaController::get_user_meta( $user_id, "printavo-status" , "disconnected" );
+		 
+		$avatar_initials = UserMetaController::get_user_meta( $user_id, "printavo-avatar_initials" );
+		
+		$avatar_url_small = UserMetaController::get_user_meta( $user_id, "printavo-avatar_url_small");
+		
+		$avatar_name = UserMetaController::get_user_meta( $user_id, "printavo-name" , Auth::user()->name );
+		
+		$avatar_background_color = UserMetaController::get_user_meta( $user_id, "printavo-avatar_background_color" , "7951B9" );
+				
+		$users_role = DB::select( "SELECT role FROM users WHERE id=" . $user_id );
+			
+		if( $avatar_initials == ""){
+			$acronym = "";
+			$words = explode(" ", $avatar_name);
+			foreach ($words as $w) {
+				$acronym .= $w[0];
+			}
+			$avatar_initials = $acronym;
+		}
+		
+		$OrdersVariables = array(
+			'printavo_email' => $email,
+			'printavo_status' => $printavo_status,
+			'avatar_name' => $avatar_name,
+			'avatar_background_color' => '#' . $avatar_background_color,
+			'avatar_url_small' => $avatar_url_small,
+			'avatar_initials' => $avatar_initials ,
+			'notification' => '',
+			'user_role'	=> (int)($users_role[0]->role)
+		);
 	
 	
 		
 		
 		
-		return view( 'tables.Archive' );
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+		return view( 'tables.Archive',$OrdersVariables );
     }
 }

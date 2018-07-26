5e60fb9f465f958d47e90a5242f56d56da3cfe3c
YoJeyThilip <thilipanyosanai@gmail.com>
2018-07-26 12:56:51 +0530

    changes

--- resources/views/tables/addons.blade.php
+++ resources/views/tables/addons.blade.php
@@ -59,7 +59,7 @@
 <script src="{{ secure_asset('js/datatables/dataTables.colVis.min.js') }}"></script>
 <script src="{{ secure_asset('js/datatables/dataTables.tableTools.min.js') }}"></script>
 <script src="{{ secure_asset( 'js/datatables/dataTables.bootstrap.min.js') }}"></script>
-<script src="{{ secure_asset( 'js/datatables/datatables.responsive.min.js') }}"></script>
+<script src="{{ secure_asset( 'js/datatables/datatables.responsive.min.js') }}"></script> 
 <script>
 $(document).ready(function() {
 	var responsiveHelper_dt_basic = undefined;

a4847405778d3f20d2bb58ad4a7697e2ad1a1d51
YoJeyThilip <thilipanyosanai@gmail.com>
2018-07-27 16:32:29 +0530

    changes

--- resources/views/tables/calculator_fabric_edit.blade.php
+++ resources/views/tables/calculator_fabric_edit.blade.php
@@ -3,6 +3,9 @@
 @section('dashboard-content')
 
 <div class="calculator_fabric_table edit_tables">
+
+<h1 class="product_calculator_header">Calculator Fabric Table Item</h1>
+
 <form action="{{ route('tables') }}?tab=calculator_fabric" method="post">
 	@csrf
 	<table id="dt_basic" class="table table-striped table-hover dataTable no-footer" width="100%" role="grid" aria-describedby="dt_basic_info" style="width: 100%;">

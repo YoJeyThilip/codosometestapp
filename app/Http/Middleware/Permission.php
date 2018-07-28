b871412e916208518b1b948ffa28488566d7d55a
YoJeyThilip <thilipanyosanai@gmail.com>
2018-07-28 12:51:40 +0530

    changes

--- app/Http/Middleware/Permission.php
+++ app/Http/Middleware/Permission.php
@@ -19,6 +19,8 @@ class Permission
      */
     public function handle($request, Closure $next, $role)
     {
+		
+		$user_id = Auth::id();
       
 		$printavo_status = UserMetaController::get_user_meta( $user_id, "printavo-status" , "disconnected" );
 		

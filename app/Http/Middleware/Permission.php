c24fe5a3304837c79f8d0493f3cc7bc1f3597bd8
YoJeyThilip <thilipanyosanai@gmail.com>
2018-07-30 12:21:53 +0530

    changes

--- app/Http/Middleware/Permission.php
+++ app/Http/Middleware/Permission.php
@@ -17,8 +17,10 @@ class Permission
 		$user_id = Auth::id();
 	
         $printavo_status = UserMetaController::get_user_meta( $user_id, "printavo-status" , "disconnected" );
+		$email = UserMetaController::get_user_meta( $user_id, "printavo-email" );
 		
 		print_r($printavo_status);
+		print_r($email);
 		
 		if( $printavo_status == "Disconnect" ) {
 			//return redirect('/settings');

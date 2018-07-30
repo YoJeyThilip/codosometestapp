5180504a98474fd76af5b32a62a28685ee583dfc
YoJeyThilip <thilipanyosanai@gmail.com>
2018-07-30 13:39:20 +0530

    changes

--- routes/web.php
+++ routes/web.php
@@ -41,22 +41,24 @@ Route::resource('/orders', 'OrderController');
 */
 
 
-Route::group([ 'middleware'=>['auth', 'Role:4'] ], function() {
 
-	Route::resource('/students', 'StudentController')->middleware(Permission::class);
-
-	Route::any('/reports', 'ReportsController@index')->name('reports')->middleware(Permission::class);
-
-	Route::any('/all_reports', 'ReportsController@all_reports')->name('all_reports');
-
-	Route::any('/resources/edit', 'ResourcesController@edit')->name('resources.edit');
-
-	Route::any('/calculator-tables', 'CalculatorTablesController@index')->name('calculator-tables');
-	
-});
 
 
 Route::group([ 'middleware'=>['Permission'] ], function() {
+	
+	Route::group([ 'middleware'=>['auth', 'Role:4'] ], function() {
+
+		Route::resource('/students', 'StudentController');
+
+		Route::any('/reports', 'ReportsController@index')->name('reports');
+
+		Route::any('/all_reports', 'ReportsController@all_reports')->name('all_reports');
+
+		Route::any('/resources/edit', 'ResourcesController@edit')->name('resources.edit');
+
+		Route::any('/calculator-tables', 'CalculatorTablesController@index')->name('calculator-tables');
+		
+	});
     
 	Route::any('/calculator', 'CalculatorController@index')->name('calculator');
 		

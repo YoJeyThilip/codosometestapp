feaf8c815a79c52f153915e7707618f6779e0c19
YoJeyThilip <thilipanyosanai@gmail.com>
2018-07-30 13:54:39 +0530

    changes

--- app/Http/Middleware/Permission.php
+++ app/Http/Middleware/Permission.php
@@ -16,13 +16,6 @@ class Permission
 				
 		$user_id = Auth::id();
         $printavo_status = UserMetaController::get_user_meta( $user_id, "printavo-status");
-		$email = UserMetaController::get_user_meta( $user_id, "printavo-email" );
-		$id = UserMetaController::get_user_meta( $user_id, "printavo_user_id" );
-		
-		print_r($printavo_status);
-		print_r($email);
-		print_r($user_id);
-		print_r($id);
 		
 		if( $printavo_status != "connected" ) {  
 			return redirect('/settings');

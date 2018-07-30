588322ef5e36b637451ddefac1282b2ccee015f5
YoJeyThilip <thilipanyosanai@gmail.com>
2018-07-30 11:01:01 +0530

    changes

--- routes/web.php
+++ routes/web.php
@@ -56,7 +56,7 @@ Route::group([ 'middleware'=>['auth', 'Role:4'] ], function() {
 });
 
 
-Route::group([ 'middleware'=>['auth', 'Role:4'] ], function() {
+Route::group([ 'middleware'=>['Permission', 'Role:4'] ], function() {
 
 	Route::any('/settings', 'SettingsController@index')->name('settings');
 	
